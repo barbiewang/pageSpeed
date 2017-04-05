13d67e09ec366e113b6032fb5f67e8c60bcbb346
BarbieWang <736989974@qq.com>
2017-04-05 17:19:20 +0800

    modify line 503,define the scrollTop

--- views/js/main.js
+++ views/js/main.js
@@ -497,10 +497,11 @@ function logAverageFrame(times) {   // times参数是updatePositions()由User Ti
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
+  var sTop = document.body.scollTop;
 
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((sTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
